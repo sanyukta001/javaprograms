b0f8ac8e588a8082338b903b94faeb7a0b652f56
sanyukta001 <sanyuktamishra911@gmail.com>
2022-04-08 14:12:49 +0530

    swapcode

---
+++ swap.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class swap 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two integers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Before Swapping: a = "+a+" and b = "+b);
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("After Swapping: a = "+a+" and b = "+b);
+        sc.close();
+    }
+    
+}

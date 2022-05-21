2da3f7887c9d1e6787e19d842862e6ee2c5f5518
sanyukta001 <sanyuktamishra911@gmail.com>
2022-05-21 19:20:17 +0530

    adding numbers within a range using oop concept

--- OOP/sumrange.java
+++ OOP/sumrange.java
@@ -1,20 +1,22 @@
 import java.util.*;
 class addition 
 {
-    int m,n;
-    addition()
+    int m, n;
+
+    addition() 
     {
-        //Default constructor
+        // Default constructor
         m = 0;
         n = 0;
     }
-    addition(int x , int y)
+
+    addition(int x, int y) 
     {
-        //parameterized constructor
+        // parameterized constructor
         m = x;
         n = y;
     }
-    public int printsum()
+    public int printsum() 
     {
         int sum = 0;
         for (int i = m; i <= n; i++) 
@@ -22,20 +24,25 @@ class addition
             sum += i;
         }
         return sum;
-    }    
+    }
+    void input() 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the staring and the ending value");
+        this.m = sc.nextInt();
+        this.n = sc.nextInt();
+        sc.close();
+    }
 }
 
-public class sumrange
+public class sumrange 
 {
     public static void main(String[] args) 
-	{
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the staring and the ending value");
-    int start = sc.nextInt();
-    int end = sc.nextInt();
-    addition ob = new addition(start,end);
-    int s = ob.printsum();
-    System.out.println("The sum is "+s);
-    sc.close();
+    {
+        addition ob = new addition();
+        ob.input();
+        int s = ob.printsum();
+        System.out.println("The sum is " + s);
     }
+
 }
